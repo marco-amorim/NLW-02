1c68b8119b5ee5d88e17cd99902b893c007d5758
marco-amorim <marcoamorim95@hotmail.com>
2020-08-05 01:59:41 -0300

    react router, TeacherForm, TeacherList and reusable component PageHeader

---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './styles.css';
+import PageHeader from '../../components/PageHeader';
+
+const TeacherList = () => {
+	return (
+		<div id="page-teacher-list" className="container">
+			<PageHeader title="Estes são os proffys disponíveis.">
+        teste
+      </PageHeader>
+		</div>
+	);
+};
+
+export default TeacherList;
