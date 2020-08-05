8b0e67967a0ddf179fab5be48e4c6cf10354529a
marco-amorim <marcoamorim95@hotmail.com>
2020-08-05 02:47:30 -0300

    TeacherItem and more styling

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 
+import './styles.css';
+
 interface PageHeaderProps {
 	title: string;
 }
@@ -21,9 +23,9 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
 
 			<div className="header-content">
 				<strong>{props.title}</strong>
-			</div>
 
-      {props.children}
+				{props.children}
+			</div>
 		</header>
 	);
 };
