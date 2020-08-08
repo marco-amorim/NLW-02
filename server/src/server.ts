9e4fabfb63a046239bf9b744744ddc1313e971c6
marco-amorim <marcoamorim95@hotmail.com>
2020-08-08 19:35:19 -0300

    controllers for db operations, queries with knex and util function

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,11 @@
 import express from 'express';
+import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-/*
-	GET: Buscar ou listar uma informacao
-	POST: Criar alguma nova informacao
-	PUT: Atualizar uma informacao existente
-	DELETE: Deletar uma informacao existente
-
-	Corpo (Request Body): Dados para criacao ou atualizacao de um registro
-	Route Params:	Identificar qual recurso eu quero atualizar ou deletar
-	Query Params: Paginacao, filtros, ordenacao
-*/
-
-app.get('/users', (req, res) => {
-	return res.json({ message: 'hello world' });
-});
+app.use(routes);
 
 app.listen(3333);
