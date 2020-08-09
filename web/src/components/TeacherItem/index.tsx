8909a5397275b6b840f7830578b81ffe38f6ebee
marco-amorim <marcoamorim95@hotmail.com>
2020-08-09 04:14:40 -0300

    connected frontend with backend

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,38 +3,53 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem = () => {
+export interface Teacher {
+	id: number;
+	avatar: string;
+	bio: string;
+	cost: number;
+	name: string;
+	subject: string;
+	whatsapp: string;
+}
+interface TeacherItemProps {
+	teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+	function createNewConnection() {
+		api.post('connections', {
+			user_id: teacher.id,
+		});
+	}
+
 	return (
 		<article className="teacher-item">
 			<header>
-				<img
-					src="https://avatars2.githubusercontent.com/u/40203788?s=460&u=bb67357c370e74a78cb43239833649004c9212d6&v=4"
-					alt="Marco Amorim"
-				/>
+				<img src={teacher.avatar} alt={teacher.name} />
 				<div>
-					<strong>Marco Amorim</strong>
-					<span>Quimica</span>
+					<strong>{teacher.name}</strong>
+					<span>{teacher.subject}</span>
 				</div>
 			</header>
 
-			<p>
-				Entusiasta das melhores tecnologias de química avançada.
-				<br /> <br />
-				Apaixonado por explodir coisas em laboratório e por mudar a vida das
-				pessoas através de experiências. Mais de 200.000 pessoas já passaram por
-				uma das minhas explosões.
-			</p>
+			<p>{teacher.bio}</p>
 
 			<footer>
 				<p>
 					Preço/hora
-					<strong>R$ 80.00</strong>
+					<strong>{teacher.cost}</strong>
 				</p>
-				<button type="button">
+				<a
+					onClick={createNewConnection}
+					href={`https://wa.me/${teacher.whatsapp}`}
+					target="_blank"
+				>
 					<img src={whatsappIcon} alt="WhatsApp" />
 					Entrar em contato
-				</button>
+				</a>
 			</footer>
 		</article>
 	);
