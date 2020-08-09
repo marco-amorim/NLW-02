ee5beb605111cf9187ec23504f5e055854c6bd70
marco-amorim <marcoamorim95@hotmail.com>
2020-08-09 20:42:50 -0300

    total connections from api

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
@@ -10,8 +10,19 @@ import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
+import api from '../../services/api';
+
 const Landing = () => {
 	const { navigate } = useNavigation();
+	const [totalConnections, setTotalConnections] = useState(0);
+
+	useEffect(() => {
+		api.get('connections').then((response) => {
+			const { total } = response.data;
+
+			setTotalConnections(total);
+		});
+	}, []);
 
 	function handleNavigateToGiveClassesPage() {
 		navigate('GiveClasses');
@@ -49,7 +60,8 @@ const Landing = () => {
 			</View>
 
 			<Text style={styles.totalConnections}>
-				Total de 285 conexões já realizadas <Image source={heartIcon} />
+				Total de {totalConnections} conexões já realizadas{' '}
+				<Image source={heartIcon} />
 			</Text>
 		</View>
 	);
